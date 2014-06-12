a4d0828ef9754bd83bcbc68b2bda4cd26c758a6a
Makabey <burgner4531@hotmail.com>
2014-06-12 13:24:09 -0400

    Debug du menu
    Faute: icone trop grande :'(

--- FichiersDuTP/assets/inc/menus.inc.php
+++ FichiersDuTP/assets/inc/menus.inc.php
@@ -30,36 +30,42 @@ function spawnMainMenu(){
 												<li><a href="galerie_videos.php">Vidéos</a></li>
 											</ul>
 										</li>
-										<li>
-											<?php
-												if((isset($_SESSION['panier'])) && (!empty($_SESSION['panier']))){
-														echo '<div><a href="gestion_panier.php">';
-														$nombreItems = count($_SESSION['panier']);
-														echo "<img src=","../images/cart.png","/>$nombreItems</a></div>";
-													}
-											?>
-										</li>
-										<li>
-											<?php
-												$return = false;
-												// Tester si un usagé est authentifié et s'il est possible de charger ses informations
-												if(isset($_SESSION['user'])){
-													$return = chargerUsager($usagertest, $_SESSION['user']);
-												}
+									</ul>
+								</li>
+								<?php echo genererMenuTopItem('apropos'); ?>
+									<a href="apropos.php"<?php if($sNomDeCettePage == 'apropos') echo ' class="aActiveFix"'; ?>>À Propos</a>
+								</li>
+								<li>
+									<?php
+										if((isset($_SESSION['panier'])) && (!empty($_SESSION['panier']))){
+												echo '<div><a href="gestion_panier.php">';
+												$nombreItems = count($_SESSION['panier']);
+												#echo '<img src="assets/images/cart.png" alt="panier" />',$nombreItems;
+												echo '</a></div>';
+											}
+									?>
+								</li>
+								<li>
+									<?php
+										$return = false;
+										// Tester si un usagé est authentifié et s'il est possible de charger ses informations
+										if(isset($_SESSION['user'])){
+											$return = chargerUsager($usagertest, $_SESSION['user']);
+										}
 
-												if($return !== false){
-													$userFullName = $_SESSION['user'];
-													echo '<span>Bonjour, ',$userFullName,'</span><img src="assets/images/arrow_down_menu.png" alt=">"/>';
-													spawnMonProfilMenu();
-												}else{
-													echo '<a href="authentify.php">Se connecter / s\'enregistrer</a>';
-												}
-												echo PHP_EOL;
-											?>
-										</li>
-								</ul>
-							</div>
-						</nav>
+										if($return !== false){
+											$userFullName = $_SESSION['user'];
+											echo '<span>Bonjour, ',$userFullName,'</span><img src="assets/images/arrow_down_menu.png" alt=">" />';
+											spawnMonProfilMenu();
+										}else{
+											echo '<a href="authentify.php">Se connecter / s\'enregistrer</a>';
+										}
+										echo PHP_EOL;
+									?>
+								</li>
+							</ul>
+						</div>
+					</nav>
 	<?php
 	} # fin de "spawnMainMenu"
 
